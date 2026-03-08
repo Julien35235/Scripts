7e45e27c2549d2e6c78c2a762a5696144d53c4e6
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-08 17:08:38 +0100

    Add files via upload

--- BASH/install_servers_minecraft.sh
+++ BASH/install_servers_minecraft.sh
@@ -8,7 +8,9 @@ sudo apt install ./jdk-21_linux-x64_bin.deb -y
 sudo mkdir /opt/minecraft
 # Rendre dans le repository de minecraft
 sudo cd /opt/minecraft
+#Téléchargement du serveur 
 sudo wget https://piston-data.mojang.com/v1/objects/4707d00eb834b446575d89a61a11b5d548d8c001/server.jar
 sudo java -Xms1024M -Xmx1024M -jar minecraft_server_1.15.2.jar nogui
-
-
+sudo sed -i 's/eula=false/eula=true/' /opt/minecraft/eula.txt
+sudo sed -e '1i java -Xmx4G -Xms4G -jar server.jar nogui' /dev/null > /opt/minecraft/start_minecraft.sh
+sudo ./start_minecraft.sh
