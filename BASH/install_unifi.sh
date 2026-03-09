6b5ceffc103d75120cb5cec4f002e10836fc49dd
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 10:09:43 +0100

    Add files via upload

---
+++ BASH/install_unifi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Mets à jour le système 
+sudo apt update && sudo apt full-upgrade -y
+#Installation de la base de données de MongoDB
+wget http://ftp.debian.org/debian/pool/main/o/openssl/libssl1.1_1.1.1w-0+deb11u1_amd64.deb -O libssl.deb && sudo dpkg -i libssl.deb
+wget https://repo.mongodb.org/apt/debian/dists/buster/mongodb-org/4.4/main/binary-amd64/mongodb-org-server_4.4.27_amd64.deb -O mongodb.deb && sudo dpkg -i mongodb.deb
+#Activation du demarrage automatique de MongoDB
+sudo systemctl enable mongodb && sudo systemctl start MongoDB
+#Installation de java 
+sudo apt install default-jdk
+#Installation Unifi 
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/ubiquiti-archive-keyring.gpg] https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list >/dev/null
+curl https://dl.ui.com/unifi/unifi-repo.gpg | sudo tee /usr/share/keyrings/ubiquiti-archive-keyring.gpg >/dev/null
+sudo apt update && sudo apt install unifi
+
+
