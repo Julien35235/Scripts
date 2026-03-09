3c6e34bcb8533fdb38afc9705fba95ee2653f00a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 09:12:08 +0100

    Add files via upload

---
+++ PowerShell/PS1/install_wsus_wds.ps1
@@ -0,0 +1,2 @@
+﻿#Installation de WSUS dans WDS
+Install-WindowsFeature -Name UpdateServices, UpdateServices-WidDB, UpdateServices-Services, UpdateServices-RSAT, UpdateServices-API, UpdateServices-UI -IncludeManagementTools
