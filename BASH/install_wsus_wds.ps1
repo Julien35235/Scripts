4d2c723a72e6840db32e33112455b7e79f547c94
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 09:11:14 +0100

    Delete BASH/install_wsus_wds.ps1

--- BASH/install_wsus_wds.ps1
+++
@@ -1,2 +0,0 @@
-﻿#Installation de WSUS dans WDS
-Install-WindowsFeature -Name UpdateServices, UpdateServices-WidDB, UpdateServices-Services, UpdateServices-RSAT, UpdateServices-API, UpdateServices-UI -IncludeManagementTools
