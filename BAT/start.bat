e6e8a60eb8ac02e633e205f66c47bc17f732434f
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 10:39:53 +0100

    Add files via upload

---
+++ BAT/start.bat
@@ -0,0 +1,8 @@
+@echo off
+start notepad.exe
+timeout /t 180
+start "C:\Program Files\Mozilla Firefox\firefox.exe"
+#start firefox.exe
+start explorer.exe
+start "C:\Users\TSSR-Julien\AppData\Local\Programs\Opera\opera.exe"
+ping google.com -n 4
