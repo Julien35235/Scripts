277236adde08f5b62455e1fdeae3eadaa2239d02
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 09:20:28 +0100

    Add files via upload

--- BASH/proxmox_menu.sh
+++ BASH/proxmox_menu.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+#!/bin/bash
+# Mets à jour le système
+#Installation de sudo
+apt install sudo -y
+sudo apt update && sudo apt full-upgrade -y
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/MacRimi/ProxMenux/main/install_proxmenux.sh)"
