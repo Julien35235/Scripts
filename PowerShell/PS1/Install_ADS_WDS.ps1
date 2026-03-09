a33bd2ce7b174b7eec7f51e8d45d59f741979113
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-09 08:50:09 +0100

    Add files via upload

---
+++ PowerShell/PS1/Install_ADS_WDS.ps1
@@ -0,0 +1,4 @@
+﻿#Installatalion de l'ADS dans WDS en PowerShell 
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName "ads.tssr.local" -InstallDNS
+Import-Module ADDSDeployment
