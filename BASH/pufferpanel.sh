2a24b2ab879b4d3f31218a785d3981407e7ef845
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-28 19:07:09 +0100

    Add files via upload

---
+++ BASH/pufferpanel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install git curl unzip nala htop wget -y 
+sudo curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
+sudo apt install pufferpanel -y 
+pufferpanel user add
+systemctl enable --now pufferpanel
