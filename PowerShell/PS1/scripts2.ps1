cb240807b0dbde199b4ac81531ab000eb23b1eb1
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-29 17:23:53 +0200

    Add files via upload

---
+++ PowerShell/PS1/scripts2.ps1
@@ -0,0 +1,18 @@
+﻿#Recuperation de l'heure actuelle
+$heure = (Get-Date -Format HH:mm:ss)
+
+#Affichage du message de bienvenue
+Write-Host "Bienvenue sur le système, $nomutilisateur@$domaine !"
+Write-host "Il est $heure."
+
+# Exécution de la commande pour obtenir l'adresse IP
+$adresse_ip = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}). IPv4Address
+write-Host "Adresse IP: $adresse_ip"
+if ($adresse_ip) {
+# Enregistrement de l'adresse IP dans le fichier ip.txt sur le bureau
+$fichier_ip = "$env:USERPROFILE\Desktop\ip.txt"
+Add-Content -Path $fichier_ip -Value "Adresse IP: $adresse_ip"
+} else {
+ write-Host "Impossible de récupérer l'adresse IP."
+ }
+
