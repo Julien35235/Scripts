feef0120dcc39a4f5df3b7671ce71d1174e08771
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-29 17:07:28 +0200

    Add files via upload

---
+++ PowerShell/PS1/scripts.ps1
@@ -0,0 +1,5 @@
+﻿Write-Output "bonjour $env:username nous somme le $(Get-Date)"
+Write-Output "Votre ordinateur est $env:COMPUTERNAME, bienvenue sur le domaine $env:userDomain"
+ipconfig  /all >  C:\Scripts\PS1\ip.txt
+Get-NetAdapter -Name "*" -Physical > C:\Scripts\PS1\ip.txt
+pause
