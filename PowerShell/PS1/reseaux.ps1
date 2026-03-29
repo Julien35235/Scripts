4ef506dee5717de61d78151917f287dd12bf3e2b
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-29 14:31:33 +0200

    Add files via upload

---
+++ PowerShell/PS1/reseaux.ps1
@@ -0,0 +1,6 @@
+﻿$adapters= Get-NetIPAddress -AddressFamily IPv4
+foreach ($adapter in $adapters) {
+Write-Host "Interface : $($adapter.InterfaceAlias)"
+Write-Host "Adresse IP : $($adapter.IPv4Address)"
+Write-Host "----------"
+}
