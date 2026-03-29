5d41f7b77e50d308b593465bb3fdf76d1dcfa200
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-29 15:33:25 +0200

    Add files via upload

---
+++ BAT/users.bat
@@ -0,0 +1,22 @@
+net user stan 123Soleil /add
+
+
+net user Directeur 123Soleil /add
+
+
+net user Vente 123Soleil /add
+
+
+net user Secretaire 123Soleil /add
+
+
+net user employel 123Soleil /add
+
+
+net user employe2 123Soleil /add
+
+e:
+mkdir Entreprise 
+cd Entreprise
+mkdir TSSR Directeur Vente Secretaire Employe1 Employe2 
+pause
