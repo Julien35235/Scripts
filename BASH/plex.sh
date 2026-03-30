723246750c4742d6ede1e054ecda587b68f75f94
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 11:09:53 +0200

    Add files via upload

--- BASH/plex.sh
+++ BASH/plex.sh
@@ -3,4 +3,5 @@ sudo apt update && sudo apt full-upgrade -y
 sudo apt install wget nala htop curl apt-transport-https -y 
 sudo wget -O- https://downloads.plex.tv/plex-keys/PlexSign.key | gpg --dearmor | sudo tee /usr/share/keyrings/plex.gpg
 echo deb [signed-by=/usr/share/keyrings/plex.gpg] https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt update
 nala install plexmediaserver -y
