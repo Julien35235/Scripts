77baca101d2bda22cf6a40c244d2abfc0191faea
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 13:40:20 +0200

    Add files via upload

---
+++ BASH/deb12to13.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Mise à jour du système Debian s
+sudo apt update && sudo apt full-upgrade -y
+sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
+sudo apt update
+sudo apt full-upgrade -y
