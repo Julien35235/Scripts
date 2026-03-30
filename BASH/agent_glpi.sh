dc69e59b917a4bbe2fe506524e41291966c06061
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 13:02:27 +0200

    Update agent_glpi.sh

--- BASH/agent_glpi.sh
+++ BASH/agent_glpi.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+# Installations des packets 
 sudo apt installl htop nala wget -y
+# Téléchargements de l'agent GLPI
 wget https://github.com/glpi-project/glpi-agent/releases/download/1.15/glpi-agent-1.15-linux-installer.pl
+# Installation et la configuration de l'agent GLPI
 sudo perl glpi-agent-linux-installer.pl
 glpi-agent –force 
 
