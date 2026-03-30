9ad0c024baea5e9e01eccbd8e27e532abf663469
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 15:16:21 +0200

    Add files via upload

--- BASH/software.sh
+++ BASH/software.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # mets à jour mon système 
-apt update 
-apt full-upgrade 
+sudo apt update && sudo apt full-upgrade 
 # installation de htop sudo nala atop pour Debian 
-sudo apt install htop sudo nala atop 
+sudo apt install htop sudo nala atop wget -y
+cd /opt
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-linux-amd64.deb
+sudo apt install ./fastfetch-linux-amd64.deb
 
