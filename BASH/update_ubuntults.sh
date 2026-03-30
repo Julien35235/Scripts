b21ac5cb6c27f2e2091e588344955c59dbe7adb5
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 10:06:04 +0200

    Add files via upload

---
+++ BASH/update_ubuntults.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+lsb_release -a
+sudo apt update && sudo apt full-upgrade -y
+sudo apt install ufw nala htop ubuntu-release-upgrader-core -y
+sudo ufw allow 1022/tcp
+sudo ufw reload
+sudo do-release-upgrade 
+sudo do-release-upgrade -d
+sudo do-release-upgrade -d
