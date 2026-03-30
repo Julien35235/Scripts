4ba04a796bfdf142f37ed8ec7f64d02fd576ff59
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-03-30 09:40:56 +0200

    Update cockpit.sh

--- BASH/cockpit.sh
+++ BASH/cockpit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update && apt full-upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 sudo apt install cockpit -y
 sudo systemctl enable cockpit.socket
 sudo systemctl start cockpit.socket
