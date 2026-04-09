f9de2b60f4c2d5de9a1de806ebb534ae28094ec0
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-09 10:18:02 +0200

    Update pkt.sh

--- BASH/pkt.sh
+++ BASH/pkt.sh
@@ -2,7 +2,7 @@
 # Mise à jour du système  
 sudo apt update && sudo apt full-upgrade -y
 # Installation des packets pour pkt
-apt install -y libglib2.0-0 libgl1-mesa-glx libxcb-xinerama0 htop nala -y
+sudo apt install libglib2.0-0 libgl1-mesa-glx libxcb-xinerama0 htop nala -y
 cd /opt
 #Télechargements du software de PKT.
 #wget https://prod-tf-ui.s3.amazonaws.com/s/ff9e491c-49be-4734-803e-a79e6e83dab1/resources/9accb7fd-7560-45c6-b6de-9ab7e9cf07b8/v1/en-US/CiscoPacketTracer_900_Ubuntu_64bit.deb
