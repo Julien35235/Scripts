562b46e9089d0cb45943c057d7e0c5572eb7e2b0
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-16 13:22:02 +0200

    Add files via upload

---
+++ PowerShell/PS1/ads_excel.ps1
@@ -0,0 +1,7 @@
+﻿#Manipuler des fichiers Excel avec PowerShell 
+Install-Module -Name ImportExcel
+#Install-Module -Name ImportExcel -Scope CurrentUser
+Get-Command -Module ImportExcel
+Get-ADUser -Filter * | Export-Excel -Path './AD-utilisateurs.xlsx'
+Get-ADUser -Filter * | Export-Excel -Path './AD-utilisateurs.xlsx' -AutoSize -WorksheetName "Domaine ads.tssr.local"
+Get-ADUser -Filter * | Select-Object SamAccountName,Enabled | Export-Excel -Path './AD-utilisateursBis.xlsx' -AutoSize -WorksheetName "Domaine ads.tssr.local"
