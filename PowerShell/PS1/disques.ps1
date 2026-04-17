f91612c136653d4fc8b47ee9c061db9aa9b3b4ef
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 09:08:17 +0200

    Add files via upload

---
+++ PowerShell/PS1/disques.ps1
@@ -0,0 +1,5 @@
+﻿Get-WmiObject -Class Win32_LogicalDisk | 
+    Select-Object -Property DeviceID, VolumeName, @{
+       label='FreeSpace'
+       expression={($_.FreeSpace/1GB).ToString('F2')}
+    }
