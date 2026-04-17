2a37ba5a2e5739fbcad243ba5a8178d3cea3ee0d
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 09:21:47 +0200

    Add files via upload

---
+++ PowerShell/PS1/invite_office365.ps1
@@ -0,0 +1,12 @@
+﻿# Installation du module de AzureAD
+Install-Module -Name AzureAD
+#Ensuite, il faut créer un objet avec les credentials et nous allons l'utiliser avec le cmdlet "Connect-AzureAD" afin d'initier une connexion 
+# avec le tenant.
+$Credentials = Get-Credential 
+$Credentials.password.MakeReadOnly()
+# Connexion à Azure
+Connect-AzureAD -Credential $Credentials
+# Creation d'un compte invité dans Office 365 
+New-AzureADMSInvitation -InvitedUserEmailAddress "usertssr@tssrlab.local" -SendInvitationMessage $true -InviteRedirectUrl "https://teams.microsoft.com/"
+#Lorsque le compte est ajouté, vous pouvez le vérifier en listant les comptes invités "guest" de votre tenant grâce au filtre sur le "UserType" :
+Get-AzureADUser -Filter "UserType eq 'Guest'"
