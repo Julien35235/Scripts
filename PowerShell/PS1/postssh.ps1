78ce8710564cf6759bc7aab072fd93b4bf8536a1
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 10:32:42 +0200

    Add files via upload

---
+++ PowerShell/PS1/postssh.ps1
@@ -0,0 +1,12 @@
+﻿#Installation du module Posh-SSH
+Install-Module -Name Posh-SSH
+$env:PSModulePath
+#Pour lister les commandes intégrées au module Posh-SSH
+Get-Command -Module Posh-SSH
+# Pour obtenir de l'aide quant à l'utilisation d'une de ces commandes
+help New-SSHSession
+# Importation du module Posh-SSH
+Import-Module Posh-SSH
+# Pour créer une connexion SSH, on s'appuiera sur la commande "New-SSHSession"
+#New-SSHSession -ComputerName "<ip>" -Port <port-ssh>
+
