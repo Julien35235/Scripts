f993d2f8a6292ff9d2b57c4fd8bb9239376abc14
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 09:27:14 +0200

    Add files via upload

---
+++ PowerShell/PS1/RDS.ps1
@@ -0,0 +1,8 @@
+﻿# Installation des rôles RDS en PowerShell
+Add-WindowsFeature RDS-RD-Server -Restart
+Import-Module RemoteDesktop
+Get-Command -Module RemoteDesktop
+# Configurer les licensing RDS
+New-RDSessionDeployment -ConnectionBroker SRV-RDS-01.tssrlab.local -SessionHost SRV-RDS-01.tssrlab.local -WebAccessServer SRV-RDS-01.tssrlab.local
+Add-RDServer -Server SRV-RDS-01.tssrlab.local-Role RDS-LICENSING -ConnectionBroker SRV-RDS-01.tssrlab.local
+
