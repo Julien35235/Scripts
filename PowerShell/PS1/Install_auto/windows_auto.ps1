4462b96a5808561a925eda4feb60e982ee15400b
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 11:39:08 +0200

    Add files via upload

---
+++ PowerShell/PS1/Install_auto/windows_auto.ps1
@@ -0,0 +1,6 @@
+﻿$isoPath = "C:\ISO\Windows.iso"
+
+Mount-DiskImage -ImagePath $isoPath
+$driveLetter = (Get-DiskImage -ImagePath $isoPath | Get-Volume).DriveLetter + ":"
+
+Start-Process "$driveLetter\setup.exe" -ArgumentList "/auto upgrade /quiet /noreboot"
