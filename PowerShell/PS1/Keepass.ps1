3b34aeddec63fdba23fe2f790468ae642f739aac
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-17 08:49:10 +0200

    Add files via upload

---
+++ PowerShell/PS1/Keepass.ps1
@@ -0,0 +1,9 @@
+﻿#Installation du module PoshKeePass
+Install-Module -Name PoshKeePass
+# l'importer le module PoshKeePass
+Import-Module PoShKeePass
+Get-Command -Module PoShKeePass
+# Enregistrer une base KeePass
+Get-KeePassDatabaseConfiguration
+#Creation de la nouvelle base de données Keepass
+New-KeePassDatabaseConfiguration -DatabaseProfileName "TSSR Databasse" -DatabasePath "C:\Users\TSSR-Julien\Documents\KBX" -UseMasterKey
