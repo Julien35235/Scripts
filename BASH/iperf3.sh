e1da582b54880d58c9e1b43f95365b089e22dd0b
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-27 18:42:36 +0200

    Add files via upload

--- BASH/iperf3.sh
+++ BASH/iperf3.sh
@@ -2,6 +2,6 @@
 # Mise à jour du système 
 sudo apt update && sudo apt full-upgrade -y
 # Installation de iperf3
-sudo apt install iperf3
+sudo apt install iperf3 -y
 # Lacement de iperf3
 iperf3 -s
