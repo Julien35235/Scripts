48b2b4f8502706d8ec678055aa727f440c0ba4cf
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-27 08:52:29 +0200

    Add files via upload

---
+++ PowerShell/PS1/script_salut.ps1
@@ -0,0 +1,7 @@
+﻿#Creation de la variable nom et age 
+$nomUtilisateur = "Julien"
+$age = 30
+
+#Afficher les variables dans la console en PowerShell,
+Write-Host "Nom d'utilisateur : $nomUtilisateur"
+Write-Host "Âge : $age"
