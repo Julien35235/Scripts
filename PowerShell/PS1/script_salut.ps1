baf620dbf04ef10b42cc0a9e0b1530f7d7378f24
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-29 09:10:10 +0200

    Add files via upload

--- PowerShell/PS1/script_salut.ps1
+++ PowerShell/PS1/script_salut.ps1
@@ -1,7 +1,12 @@
 ﻿#Creation de la variable nom et age 
 $nomUtilisateur = "TSSR"
 $age = 30
-
 #Afficher les variables dans la console en PowerShell.
 Write-Host "Nom d'utilisateur : $nomUtilisateur"
 Write-Host "Âge : $age"
+$nomUtilisateur = Read-Host "Entrez votre prénom"
+$age = Read-Host "Entrez votre age"
+#affichage du message de bienvenu
+Write-Output "Bonjour $nomUtilisateur, tu as $age ans."
+
+
