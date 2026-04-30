8dba0401224471bf7d3059ae3406f7a5ea649e73
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-30 16:46:32 +0200

    Add files via upload

---
+++ PowerShell/PS1/ssid_users.ps1
@@ -0,0 +1,8 @@
+﻿#Récupérations des SID des utilisateurs
+Get-WmiObject -Class win32_UserAccount | Format-Table Name,SID
+(Get-WmiObject -Class win32_UserAccount -Filter "name='usertssr' and domain='$env:ComputerName'").SID
+Get-CimInstance -Class win32_UserAccount | Format-Table Name,SID
+Get-LocalUser | Select-Object Name, SID
+Get-ADUser -Filter * | Select-Object Name, SID
+wmic useraccount get name,sid
+whoami /user
