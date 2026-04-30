af07be019fcd1a76ce349b85fc1e232d16f4a627
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-30 15:15:22 +0200

    Add files via upload

---
+++ PowerShell/PS1/notification_tssr.ps1
@@ -0,0 +1,8 @@
+﻿#Installion du module Burnt Toast
+Install-Module BurntToast
+#Ensuite, on peut en profiter pour lister les cmdlets disponibles
+Get-Command -Module BurntToast
+Set-BTFunctionLevel Advanced
+New-BTAppId -AppId "TSSR notification"
+New-BurntToastNotification
+New-BurntToastNotification -AppId "TSSR notification" -AppLogo "C:\png\logo.png" -Text "PowerShell Notification" , "This module is very cool ! :-)"
