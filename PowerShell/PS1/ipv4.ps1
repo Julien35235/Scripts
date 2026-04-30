f9e7add663afbc902007f4763922e3d0b0ed5290
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-04-30 15:24:29 +0200

    Add files via upload

---
+++ PowerShell/PS1/ipv4.ps1
@@ -0,0 +1,7 @@
+﻿#Configuration d’une nouvelle adresse IP
+New-NetIPAddress –InterfaceIndex 12 –IPAddress 10.135.95.2 –PrefixLength 24 –DefaultGateway 10.235.95.1
+Set-NetIPInterface -InterfaceIndex 12 -Dhcp {Enabled/Disabled}
+# Ajout des DNS
+Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses 8.8.8.8
+Set-DnsClientServerAddress -InterfaceIndex 12 -ServerAddresses 10.235.95.1
+Get-NetIPConfiguration
---
+++ PowerShell/PS1/os.ps1
@@ -0,0 +1,8 @@
+﻿#Recuperation des informations de l'OS
+Get-WmiObject Win32_OperatingSystem
+Get-WmiObject Win32_OperatingSystem | gm -MemberType Property
+Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version, ServicePackMajorVersion, OSArchitecture, CSName, WindowsDirectory, NumberOfUsers, BootDevice
+$OSInfo = Get-WmiObject Win32_OperatingSystem | Select-Object Caption, Version, ServicePackMajorVersion, OSArchitecture, CSName, WindowsDirectory, NumberOfUsers, BootDevic
+$OSInfo.OSArchitecture
+Get-WmiObject SoftwareLicensingService | Select-Object OA3xOriginalProductKey
+Get-WmiObject SoftwareLicensingService -ComputerName FloHypervisor,FloStation
