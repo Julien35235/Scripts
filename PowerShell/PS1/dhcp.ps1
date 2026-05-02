618d990c6487f289f1a0ab4e4bd124eae3b230f2
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-02 14:59:16 +0200

    Update dhcp.ps1

--- PowerShell/PS1/dhcp.ps1
+++ PowerShell/PS1/dhcp.ps1
@@ -8,7 +8,7 @@ Restart-Service dhcpserver
 Add-DhcpServerv4Scope -Name "Clients" -StartRange 10.235.95.10 -EndRange 10.235.95.100 -SubnetMask 255.255.255.0
 #On définit la passerelle par défaut  10.235.95.1, grâce à l'option DHCP
 Set-DhcpServerv4OptionDefinition -OptionId 3 -DefaultValue 10.235.95.1
-##On définit le DNS par défaut  10.235.95.2, grâce à l'option DHCP
+#On définit le DNS par défaut  10.235.95.2, grâce à l'option DHCP
 Set-DhcpServerv4OptionDefinition -OptionId 6 -DefaultValue 10.235.95.2
 Set-DhcpServerv4Scope -ScopeId 172.16.0 -Name "Clients" -State Active
 #Afin de vérifier que la configuration est bien prise en compte, on peut lister les scopes DHCP du serveur 
