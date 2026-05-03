e9c2893974515bf532adca18e9fba15153c1ede0
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-03 14:48:49 +0200

    Update pve9.sh

--- BASH/pve9.sh
+++ BASH/pve9.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Ajout du dépôt de Proxmox à la liste des sources
 cat > /etc/apt/sources.list.d/pve-install-repo.sources << EOL
 Types: deb
 URIs: http://download.proxmox.com/debian/pve
@@ -6,7 +7,11 @@ Suites: trixie
 Components: pve-no-subscription
 Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 EOL
+#Mise à jour du système
 sudo apt update && apt full-upgrade -y
-sudo apt install proxmox-default-kernel proxmox-ve postfix open-iscsi chrony
+#Installation de Proxmox VE 
+sudo apt install proxmox-default-kernel proxmox-ve postfix open-iscsi chrony -y
+# Mise à jour de GRUB pour appliquer les changements de noyau
 update-grub
+# Redémarrage du système pour appliquer les modifications
 sudo reboot
