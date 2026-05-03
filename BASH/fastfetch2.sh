b95781bc67bc1150a5300fffd1e818f5b64fb486
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-03 23:45:01 +0200

    Add files via upload

---
+++ BASH/fastfetch2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo apt update && sudo apt full upgrade -y
+#installation de wget 
+sudo apt install wget curl htop nala -y
+#se déplacer dans opt
+cd /opt
+#Recuperation de fastfetch en utilisant la commande wget
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.62.1/fastfetch-linux-amd64.deb
+#installation de fastfetch 
+apt install ./fastfetch-linux-amd64.deb -y
