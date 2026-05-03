94a5fde8efeab3399561a3dba80ea83ad1be9f2a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-03 12:24:18 +0200

    Update pve8to9.sh

--- BASH/pve8to9.sh
+++ BASH/pve8to9.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#Mise a jour du système
 sudo apt update && sudo apt full-upgrade
+#Test si le serveur est compatible avec PVE9
 pve8to9
-sed -i 's/bookworm/trixie/g' /etc/apt/sources.list 
+#Remplacement des dépôts de Debian
+sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
+#Mise a jour du système
 sudo apt update 
 sudo apt full-upgrade
+#Redemarrage du serveur 
 sudo reboot
