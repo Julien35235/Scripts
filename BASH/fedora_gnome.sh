b4255f991e3408396955739fdaeb491c8da60d10
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-03 18:01:34 +0200

    Add files via upload

---
+++ BASH/fedora_gnome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo dnf update
+sudo dnf upgrade -y
+#Installation de l'enviromment bureau de Gnome dans Debian
+sudo dnf groupinstall gnome -y
+sudo dnf install gnome-tweak-tool -y
+sudo systemctl stop sddm.service && systemctl disable sddm.service
+sudo systemctl start gdm.service && systemctl enable gdm.service
+sudo reboot
