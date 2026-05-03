1e8ff1ee2731381136768c56205e16ae18b0ffb8
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-03 14:58:47 +0200

    Update deb12to13.sh

--- BASH/deb12to13.sh
+++ BASH/deb12to13.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Mise à jour du système Debian s
+#Mise à jour du système Debian
 sudo apt update && sudo apt full-upgrade -y
 sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
 sudo apt update
