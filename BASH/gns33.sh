6892034b3a0cdb8e416b909c0811fa5d99a551e2
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-05 21:06:12 +0200

    Add files via upload

---
+++ BASH/gns33.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo apt update && sudo apt full upgrade -y
+#Ajouter le dépot de GNS33 au système 
+sudo add-apt-repository ppa:gns3/ppa
+#Mise à jour du système
+sudo apt update        
+#Installation de GNS33                        
+sudo apt install gns3-gui gns3-server
