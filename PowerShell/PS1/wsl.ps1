0c21c3a44acda6b3d50fd14a29a5d6b60fae0ebc
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-05 09:26:13 +0200

    Add files via upload

---
+++ PowerShell/PS1/wsl.ps1
@@ -0,0 +1,8 @@
+﻿#Activation de Virtual Machine Platform 
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+#Activation de Windows Subsystem for Linux
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+#Installation de WSL 
+wsl.exe --install
+wsl.exe --set-default-version 2
+wsl --set-version kali-linux 2
