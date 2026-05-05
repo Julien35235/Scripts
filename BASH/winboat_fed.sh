681ea6d371a8ed01678a493e23982b687771397d
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-05 14:08:21 +0200

    Add files via upload

---
+++ BASH/winboat_fed.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo dnf update
+sudo dnf upgrade -y
+#Installation des packets nécessaire pour Docker et wget
+sudo dnf install docker.io wget -y
+#Activation du démarrage automatique de docker
+sudo systemctl enable docker
+#Démarrage de Docker
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+#Installation de freerdp3
+sudo dnf install freerdp3-x11 -y
+cd /opt
+wget https://github.com/TibixDev/winboat/releases/download/v0.9.0/winboat-0.9.0-amd64.deb
+dnf install ./winboat-0.9.0-amd64.deb -y
+
