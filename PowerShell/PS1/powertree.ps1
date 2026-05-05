4d4e29152dfc066e9f8e001ffebdfed0f77d4b4a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-05 09:47:06 +0200

    Add files via upload

---
+++ PowerShell/PS1/powertree.ps1
@@ -0,0 +1,6 @@
+﻿#Installation du Module de PowerTree
+Install-Module -Name PowerTree -Scope CurrentUser
+Get-Command -Module PowerTree
+#Affichage de l'arborescence d'un répertoire spécifique
+Show-PowerTree
+Show-PowerTree -LiteralPath "C:\Windows\System32\WindowsPowerShell"
