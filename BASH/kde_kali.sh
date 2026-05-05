66f48f15faffc6845a0ccea4f1d42829d779814a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-05 09:25:41 +0200

    Add files via upload

---
+++ BASH/kde_kali.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo apt update && sudo apt full upgrade -y
+#Installation de l’environnement de bureau GNOME
+sudo apt install kali-desktop-kde -y
+#Redemarrage du système
+sudo reboot
