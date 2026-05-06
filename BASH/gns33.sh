751d3b85b8ba205576d6f20a164d1b6e297ebdc4
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-06 10:37:09 +0200

    Update gns33.sh

--- BASH/gns33.sh
+++ BASH/gns33.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Mise à jour du système 
 sudo apt update && sudo apt full-upgrade -y
+#Installation des dépendances nécessaires
+sudo apt install -y ca-certificates curl gnupg lsb-release
 #Ajouter le dépot de GNS33 au système 
 sudo add-apt-repository ppa:gns3/ppa
 #Mise à jour du système
