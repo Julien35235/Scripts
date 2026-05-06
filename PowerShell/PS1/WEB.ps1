639542a912333585a151216b601792cdd184d29e
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-06 11:13:44 +0200

    Add files via upload

---
+++ PowerShell/PS1/WEB.ps1
@@ -0,0 +1,5 @@
+﻿#Installation du serveur WEB de ILS
+Install-WindowsFeature Web-Server -IncludeManagementTools
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+#Activation du démarrage automatique du serveur WEB
+Set-Service -name W3SVC -startupType Automatic
