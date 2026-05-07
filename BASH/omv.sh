97ceb238f735d57dc3d49c886ebd8c210028ad4a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-07 14:51:07 +0200

    Add files via upload

---
+++ BASH/omv.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo apt update && sudo apt full upgrade -y
+#Installation des packets nécessaire 
+apt-get install --yes gnupg wget -y
+#Ajouter la clé GPG au système
+cat <<EOF >> /etc/apt/sources.list.d/openmediavault.list
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public/ synchrony main
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.io/ synchrony main
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages/ synchrony main
+# Uncomment the following line to add software from the proposed repository.
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public/ synchrony-proposed main
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.io/ synchrony-proposed main
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages/ synchrony-proposed main
+# This software is not part of OpenMediaVault, but is offered by third-party
+# developers as a service to OpenMediaVault users.
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.org/public/ synchrony partner
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://packages.openmediavault.io/ synchrony partner
+deb [signed-by=/usr/share/keyrings/openmediavault-archive-keyring.gpg] http://downloads.sourceforge.net/project/openmediavault/packages/ synchrony partner
+EOF
+export LANG=C.UTF-8
+export DEBIAN_FRONTEND=noninteractive
+export APT_LISTCHANGES_FRONTEND=none
+apt-get update
+apt-get --yes --auto-remove --show-upgraded \
+    --allow-downgrades --allow-change-held-packages \
+    --no-install-recommends \
+    --option DPkg::Options::="--force-confdef" \
+    --option DPkg::Options::="--force-confold" \
+    install openmediavault
+
+omv-confdbadm populate
+omv-salt deploy run systemd-networkd
