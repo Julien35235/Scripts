792fd5244894172f9988b732899086d0251482db
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-11 20:34:13 +0200

    Add files via upload

---
+++ PowerShell/PS1/update_wds2025.ps1
@@ -0,0 +1,3 @@
+﻿#Activation de la fonctionnalité dans le Registre Windows pour passer sous Windows Server 2025
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AllowWindowsServerFeatureUpdate"
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AllowWindowsServerFeatureUpdate" -Name "AllowWindowsServerFeatureUpdate" -PropertyType DWord -Value 1
