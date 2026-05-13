95b0bcf818c8e81f53c583a9c4bbd74e7ed0c5c8
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-13 21:07:54 +0200

    Add files via upload

--- BASH/qemu_agent_deb.sh
+++ BASH/qemu_agent_deb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Mise à jour du système 
-sudo apt update && sudo apt full upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 #Installation du QEMU Guest Agent 
 sudo apt install qemu-guest-agent -y
 #Une fois installé, démarrez le service et activez-le au démarrage
