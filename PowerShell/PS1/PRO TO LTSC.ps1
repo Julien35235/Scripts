a657845c5913c74d951bec0dad607c5d03978517
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-14 20:16:47 +0200

    Add files via upload

---
+++ PowerShell/PS1/PRO TO LTSC.ps1
@@ -0,0 +1,12 @@
+﻿# Chemin de la clé de Registre à modifier
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
+# Modifier les valeurs avec les nouvelles informations
+Set-ItemProperty -Path $regPath -Name EditionID -Value "EnterpriseS"
+Set-ItemProperty -Path $regPath -Name ProductName -Value "Windows 10 Enterprise LTSC"
+Set-ItemProperty -Path $regPath -Name ReleaseID -Value "21H2"
+Set-ItemProperty -Path $regPath -Name DisplayVersion -Value "21H2"
+Set-ItemProperty -Path $regPath -Name CurrentBuild -Value "19044"
+Set-ItemProperty -Path $regPath -Name CurrentBuildNumber -Value "19044"
+
+Write-Host "Modifications du registre effectuées. Ne redémarrez pas la machine avant d'avoir lancé l'installation." -ForegroundColor Green
---
+++ PowerShell/PS1/script_config EPNAK JDE.ps1
@@ -0,0 +1,31 @@
+﻿# vider le cache DNS 
+ipconfig /flushdns
+
+# afficher les configurations réseaux
+ipconfig 
+ipconfig /all
+Get-NetAdapter
+Get-NetTCPConnection
+#Recuperation de notre IPV4 publique 
+Invoke-RestMethod -Uri ('https://ipinfo.io/')
+
+# Testes en utiliant la commande ping et Test-Connection  
+ping 10.235.95.253
+ping 10.235.95.248
+ping 10.235.95.7
+ping 10.235.95.1
+ping google.fr
+ping pad-epnak.com
+Test-Connection 10.235.95.248
+
+
+# Voir les DNS en IPV4 en utilisant la commande nslookup
+nslookup google.fr
+nslookup pad-epnak.com
+
+# Il permet de suivre le chemin emprunté par un paquet IP (Internet Protocol) pour arriver à sa destination
+Tracert pad-epnak.com
+Tracert google.fr
+
+
+
