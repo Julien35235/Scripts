9a5c94a858fedbf9122e50cb45e64cfd4d68a73f
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-14 20:46:35 +0200

    Add files via upload

---
+++ PowerShell/PS1/RDP.ps1
@@ -0,0 +1,6 @@
+﻿#Activation du bureau à distance RDP 
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Bureau à distance"
+Get-NetFirewallRule -DisplayGroup "Bureau à distance"
+#Redemarage du service RDP
+Restart-Service TermService -Force
