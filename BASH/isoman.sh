76891e78fa25f51704e61e7152cd9d6808095cff
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 11:39:08 +0200

    Update isoman.sh

--- BASH/isoman.sh
+++ BASH/isoman.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+#Installation de docker et de GIT
 sudo apt install docker-compose docker.io python3-pip htop nala git -y
 cd /opt
+#Creation du répetoire de Docker
 mkdir docker 
-cd docker 
+cd docker
+#Clonnage du repository de Isoman
 sudo git clone https://github.com/aloks98/isoman.git
 cd isoman
+#lancement du conteneur de ISOMAN
 docker compose up -d
