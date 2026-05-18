68c02b2fc658562e6646f700be3018ea617a25a6
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 11:44:22 +0200

    Update runtipi.sh

--- BASH/runtipi.sh
+++ BASH/runtipi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#Mise à jour du système 
 sudo apt update && sudo apt full-upgrade -y
+#Installation de wget curl
 sudo apt installl htop nala curl wget -y
+#Installation du Cloud de RunTipi
 sudo curl -L https://setup.runtipi.io | bash
