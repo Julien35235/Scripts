22fb5efb34ebc3400cc370b1287a0bb193413807
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 11:25:16 +0200

    Update debian_kde_plasma.sh

--- BASH/debian_kde_plasma.sh
+++ BASH/debian_kde_plasma.sh
@@ -3,4 +3,5 @@
 sudo apt update && sudo apt full upgrade -y
 #Installation de l'enviromment bureau de KDE Plasma Desktop dans Debian
 sudo apt install install kde-plasma-desktop -y
+#Redemmarage du système
 sudo reboot
