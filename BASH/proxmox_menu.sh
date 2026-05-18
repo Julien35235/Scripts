e87ef3da43eae66fd1d5c5fe2f8f5fa68b96f55c
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 10:26:00 +0200

    Update proxmox_menu.sh

--- BASH/proxmox_menu.sh
+++ BASH/proxmox_menu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Mets à jour le système
-#Installation de sudo
-apt install sudo -y
+# Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+#Installation de Proxmox_menu
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/MacRimi/ProxMenux/main/install_proxmenux.sh)"
