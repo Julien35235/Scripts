342e52916bdea2940b745121b38256df6279c682
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 14:36:44 +0200

    Update winboat_deb.sh

--- BASH/winboat_deb.sh
+++ BASH/winboat_deb.sh
@@ -11,6 +11,8 @@ sudo usermod -aG docker $USER
 #Installation de freerdp3
 sudo apt install freerdp3-x11 -y
 cd /opt
+#Recuperation de winboat en utilisant la commande wget
 wget https://github.com/TibixDev/winboat/releases/download/v0.9.0/winboat-0.9.0-amd64.deb
+#Installation de Winboat
 apt install ./winboat-0.9.0-amd64.deb -y
 
