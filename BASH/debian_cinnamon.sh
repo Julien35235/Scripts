906809973da7fc020d24dfdd397e85adca4ad798
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 11:23:03 +0200

    Add files via upload

---
+++ BASH/debian_cinnamon.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Mise à jour du système 
+sudo apt update && sudo apt full upgrade -y
+#Installation de l'enviromment bureau de Cinanamon dans Debian
+sudo apt install cinnamon -y
+#Redemmarage du système 
+sudo reboot
