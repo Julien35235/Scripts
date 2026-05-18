70aaeaf5ea3755a8ff577be35c7c5726913b6bfe
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 15:20:51 +0200

    Add files via upload

--- PowerShell/PS1/update_wds2025.ps1
+++ PowerShell/PS1/update_wds2025.ps1
@@ -1,3 +1,5 @@
 ﻿#Activation de la fonctionnalité dans le Registre Windows pour passer sous Windows Server 2025
 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AllowWindowsServerFeatureUpdate"
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AllowWindowsServerFeatureUpdate" -Name "AllowWindowsServerFeatureUpdate" -PropertyType DWord -Value 1
+#Cette commande affiche les informations de licence de Windows et montre si Windows est activé et le type de licence utilisé.
+slmgr /dli
