53ebbdead7e728a267a14eca0511597c182761fd
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 16:45:33 +0200

    Update smb.sh

--- BASH/smb.sh
+++ BASH/smb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
-#Installation du server de samba
-sudo apt install samba -y
+#Installation du server de samba 
+sudo apt install cifs-utils samba -y
 systemctl enable smbd.service
 systemctl start smbd.service
