1a3159fbe0e74b66fb2883e8debe6ef7c7b2185d
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 14:18:53 +0200

    Update pufferpanel.sh

--- BASH/pufferpanel.sh
+++ BASH/pufferpanel.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install git curl unzip nala htop wget -y 
+#Installation de git curl ZIP
+sudo apt install git curl unzip nala htop wget -y
+#Installation de PufferPanel
 sudo curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
 sudo apt install pufferpanel -y 
+#Ajoute un utilisateur avec PufferPanel
 pufferpanel user add
+#Activation du demarrage du service de PufferPanel
 sudo systemctl enable --now pufferpanel
