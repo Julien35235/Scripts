a881f9c0955b545bfcb9f5f3ff3b870806d5da8c
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 11:25:06 +0200

    Update debian_kde_full.sh

--- BASH/debian_kde_full.sh
+++ BASH/debian_kde_full.sh
@@ -3,4 +3,5 @@
 sudo apt update && sudo apt full upgrade -y
 #Installation de l'enviromment bureau de KDE full dans Debian
 sudo apt install install kde-full -y
+#Redemmarage du système
 sudo reboot
