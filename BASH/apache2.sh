655e521f56295e7f7644ca3c5b79e1461adf1cc2
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 15:41:41 +0200

    Update apache2.sh

--- BASH/apache2.sh
+++ BASH/apache2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+#Installation du serveur WEB avec Apache2
 sudo apt installl htop nala apache2 -y
+#Activation du service de démarrage du serveur WEB Apache2 
 sudo systemctl enable apache2
+#Demarrage du serveur WEB Apache2 
 sudo systemctl start apache2
