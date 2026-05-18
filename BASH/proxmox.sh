116c3d3187b5432c8cbe5011be81e66ce2ae9bfc
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 14:23:29 +0200

    Update proxmox.sh

--- BASH/proxmox.sh
+++ BASH/proxmox.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#Affichage de l'adresse IP de la machine
 hostname --ip-address
-apt update && apt full-upgrade
+#Mise à jour du système
+sudo apt update && sudo apt full-upgrade
+#Installation des outils nécessaires pour ajouter des dépôts sécurisés
 sudo apt install curl software-properties-common apt-transport-https ca-certificates gnupg2 
+#Ajoute le dépot de Proxmox au système
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >
 /etc/apt/sources.list.d/pve-install-repo.list
-apt update && apt full-upgrade
-apt install proxmox-default-kernel proxmox-ve postfix open-iscsi chrony-y
+#Mise à jour de la liste des logiciels après ajout du dépôt
+sudo apt update 
+#Installation de Proxmox VE 
+sudo apt install proxmox-default-kernel proxmox-ve postfix open-iscsi chrony-y
+#Mise à jour du menu de démarrage du système
 update-grub
