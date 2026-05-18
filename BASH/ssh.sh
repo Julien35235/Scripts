78b25f3a7a03ffbc9adbb4b5e10593905b7e27c9
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 16:17:08 +0200

    Update ssh.sh

--- BASH/ssh.sh
+++ BASH/ssh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && apt full-upgrade -y
+#Installation du serveur ssh
 sudo apt install openssh-server ssh -y
+#Activation du service ssh en automatique
 sudo systemctl enable ssh 
+#Demarrage du service ssh
 sudo systemctl start ssh
  
