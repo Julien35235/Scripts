dc1212bfefb9e7cd3d88acb4118d45c14839b95e
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 14:42:07 +0200

    Update cockpit.sh

--- BASH/cockpit.sh
+++ BASH/cockpit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+#Installation de cockpit
 sudo apt install cockpit -y
+#Activation du démarrage automatique de cockpit
 sudo systemctl enable cockpit.socket
+#Demmarge du service de cockpit
 sudo systemctl start cockpit.socket
+
 
