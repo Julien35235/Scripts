48c577aeb9928d82f94e2671c63d62b5ea7d1762
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-18 16:31:18 +0200

    Add files via upload

---
+++ PowerShell/PS1/admin.ps1
@@ -0,0 +1,4 @@
+﻿#Exécuter un script PowerShell en tant qu’administrateur
+powershell.exe Start-Process powershell -Verb runAs
+pwsh.exe -Command Start-Process pwsh -Verb runAs
+Start-Process powershell -Verb runAs
