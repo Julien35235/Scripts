9532ebb97d376775b9693817ad5a4ced9396a74a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-25 17:50:00 +0200

    Update jellyfin.sh

--- BASH/jellyfin.sh
+++ BASH/jellyfin.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#Mise à jour du système
 sudo apt update && sudo apt full-upgrade -y
+#Installation des packets
 sudo apt install wget nala htop curl apt-transport-https sudo -y 
+#Installation du serveur de Multimedia
 wget -O- https://repo.jellyfin.org/install-debuntu.sh |  bash
+#Activation du demarrage automatique du serveur de multimedia
 sudo systemctl enable --now jellyfin.service
