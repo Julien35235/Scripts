ee5095c74fbb0622c40e29d6af976de79492b750
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-25 16:04:46 +0200

    Update fastfetch2.sh

--- BASH/fastfetch2.sh
+++ BASH/fastfetch2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Mise à jour du système 
-sudo apt update && sudo apt full upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 #installation de wget 
 sudo apt install wget curl htop nala -y
 #se déplacer dans opt
