67839e94e6d6a9ccb12f9f96d57949e4c2a82c8e
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-26 09:35:24 +0200

    Add files via upload

--- BASH/winboat_deb.sh
+++ BASH/winboat_deb.sh
@@ -2,12 +2,18 @@
 #Mise à jour du système 
 sudo apt update && sudo apt full-upgrade -y
 #Installation des packets nécessaire pour Docker et wget
-sudo apt install docker.io wget -y
+sudo apt install docker.io wget apt-transport-https ca-certificates curl gnupg2 -y
+sudo usermod -aG docker $USER
+#Ajouter le dépôt officiel Docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+#Mise à jour du système
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
 #Activation du démarrage automatique de docker
 sudo systemctl enable docker
 #Démarrage de Docker
 sudo systemctl start docker
-sudo usermod -aG docker $USER
 #Installation de freerdp3
 sudo apt install freerdp3-x11 -y
 cd /opt
