d8bbbadda4926d14fc8b9ab696805b407673b011
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-26 10:20:06 +0200

    Add files via upload

--- BAT/routages.bat
+++ BAT/routages.bat
@@ -1,4 +1,6 @@
-#Affichage de la table de routage 
+#Affichage de la table de routage en IPV4 et IPV6
+route PRINT -4
+route PRINT -6
 route print
 #Ajouter une route statique à Windows
 route add 192.168.100.0 MASK 255.255.255.0 10.10.20.1
@@ -6,3 +8,4 @@ route add 192.168.100.0 MASK 255.255.255.0 10.10.20.1
 route add -p 192.168.100.0 MASK 255.255.255.0 10.10.20.1
 #Affichage de la table de routage 
 route print
+
