a32b79aca2f6cacc6973738006d6defc0b67faf3
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-26 10:15:20 +0200

    Update fedora_gnome.sh

--- BASH/fedora_gnome.sh
+++ BASH/fedora_gnome.sh
@@ -2,7 +2,7 @@
 #Mise à jour du système 
 sudo dnf update
 sudo dnf upgrade -y
-#Installation de l'enviromment bureau de Gnome dans Debian
+#Installation de l'enviromment bureau de Gnome dans Fedora
 sudo dnf groupinstall gnome -y
 sudo dnf install gnome-tweak-tool -y
 sudo systemctl stop sddm.service && systemctl disable sddm.service
