f585e11273170fbef291622fc91e7216e61c6fe7
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-27 15:22:19 +0200

    Update iVentoy.sh

--- BASH/iVentoy.sh
+++ BASH/iVentoy.sh
@@ -7,7 +7,7 @@ cd /opt
 #Recuperation de iVentoy depuis GitHub en utilisant la commande wget
 wget https://github.com/ventoy/PXE/releases/download/v1.0.21/iventoy-1.0.21-linux-free.tar.gz
 #Extration de l'archive de iVentoy avec la commande tar
-tar xzvf iventoy-1.0.19-linux-free.tar.gz
+tar xzvf iventoy-1.0.21-linux-free.tar.gz
 #Suppersion de l'archive de iVentoy 
 rm iventoy-1.0.19-linux-free.tar.gz
 #Rennomage de l'archive de iVentoy
