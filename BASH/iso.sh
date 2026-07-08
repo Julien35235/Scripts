7154020d16aeb36fa1d0205e6b05da0b11001d10
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-08 16:36:48 +0200

    Add files via upload

---
+++ BASH/iso.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# 1. Définition du répertoire cible
+TARGET_DIR="/opt/iventoy/iso"
+
+# 2. Vérification et sécurisation du dossier
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "[ERREUR] Le répertoire $TARGET_DIR n'existe pas. Création en cours..."
+    mkdir -p "$TARGET_DIR" || { echo "Impossible de créer le dossier. Fin du script."; exit 1; }
+fi
+
+cd "$TARGET_DIR" || { echo "Impossible d'accéder à $TARGET_DIR. Fin du script."; exit 1; }
+
+# 3. Liste des URL des ISO à télécharger (facile à mettre à jour)
+URLS=(
+    "https://releases.ubuntu.com/resolute/ubuntu-26.04-desktop-amd64.iso"
+    "https://releases.ubuntu.com/resolute/ubuntu-26.04-live-server-amd64.iso"
+    "https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-13.5.0-amd64-netinst.iso"
+    "https://download.fedoraproject.org/pub/fedora/linux/releases/44/Workstation/x86_64/iso/Fedora-Workstation-Live-44-1.7.x86_64.iso"
+    "https://download.fedoraproject.org/pub/fedora/linux/releases/44/Server/x86_64/iso/Fedora-Server-dvd-x86_64-44-1.7.iso"
+    "https://repo.almalinux.org/almalinux/10/live/x86_64/AlmaLinux-10.2-x86_64-Live-GNOME.iso"
+    "https://repo.almalinux.org/almalinux/10/live/x86_64/AlmaLinux-10.2-x86_64-Live-KDE.iso"
+    "https://cdimages.ubuntu.com/edubuntu/releases/26.04/release/edubuntu-26.04-desktop-amd64.iso"
+    "https://mirrors.univ-reims.fr/IMAGES/zorinos/18/Zorin-OS-18.1-Core-64-bit.iso"
+    "https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.2-x86_64-dvd1.iso"
+    "https://download.rockylinux.org/pub/rocky/10/isos/x86_64/Rocky-10.2-x86_64-minimal.iso"
+    "https://iso.pop-os.org/24.04/amd64/generic/25/pop-os_24.04_amd64_generic_25.iso"
+    "https://kali.download/base-images/kali-2026.2/kali-linux-2026.2-installer-netinst-amd64.iso"
+    "https://pub.linuxmint.io/stable/22.3/linuxmint-22.3-cinnamon-64bit.iso"
+    "https://pub.linuxmint.io/stable/22.3/linuxmint-22.3-xfce-64bit.iso"
+    "https://pub.linuxmint.io/stable/22.3/linuxmint-22.3-mate-64bit.iso"
+    "https://mirror.stream.centos.org/10-stream/BaseOS/x86_64/iso/CentOS-Stream-10-20260707.1-x86_64-dvd1.iso"
+)
+
+# 4. Boucle de téléchargement intelligente
+echo "Début du téléchargement des ISO dans $TARGET_DIR..."
+for url in "${URLS[@]}"; do
+    filename=$(basename "$url")
+    echo "--------------------------------------------------"
+    echo "Vérification / Téléchargement de : $filename"
+    
+    # -c : Reprend le téléchargement si interrompu
+    # --no-verbose : Évite de spammer le terminal tout en affichant la progression fondamentale
+    wget -c --no-verbose "$url"
+done
+
+echo "--------------------------------------------------"
+echo "Tous les téléchargements sont terminés !"
