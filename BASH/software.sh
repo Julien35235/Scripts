518c944925f62ce1b87ff7cce823a10f9d22a16a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-15 10:33:00 +0200

    Update software.sh

--- BASH/software.sh
+++ BASH/software.sh
@@ -6,5 +6,3 @@ sudo apt install htop sudo nala atop wget -y
 cd /opt
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.61.0/fastfetch-linux-amd64.deb
 sudo apt install ./fastfetch-linux-amd64.deb
-
-
