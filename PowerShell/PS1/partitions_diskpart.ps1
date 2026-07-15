8b978046a7a1b3e320c423c4f69bfedcc76029e8
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-15 16:25:02 +0200

    Update partitions_diskpart.ps1

--- PowerShell/PS1/partitions_diskpart.ps1
+++ PowerShell/PS1/partitions_diskpart.ps1
@@ -1,8 +1,7 @@
-#Recuperation de l'espace de disque
+#Recuperation de l'espace de disque avec diskpart
 reagentc /disable
 diskpart
 select disk 0
 select partition 4
 detail partition
 #delete partition override
-
