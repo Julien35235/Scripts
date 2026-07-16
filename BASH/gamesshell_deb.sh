54a82173a4ebe2b2c85833e84ee4dfcacfd85ac7
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-16 12:58:40 +0200

    Add files via upload

---
+++ BASH/gamesshell_deb.sh
@@ -0,0 +1,15 @@
+#Mise à jour du système 
+sudo apt update && sudo apt full-upgrade -y
+#Installations des dépendances 
+sudo apt install git nala ssh wget curl -y
+#Se rendre dans le répertoire /opt 
+cd /opt
+#Creation du répertoire de gameshell
+mkdir gameshell
+cd gameshell
+#Recuperation du jeu de GamesShell 
+wget https://github.com/phyver/GameShell/releases/download/v0.6.0/gameshell.sh
+#Rendre exécutables 
+sudo chmod +x gameshell.sh
+#Lancement du jeu 
+./gameshell.sh
