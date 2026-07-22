929aa0b81c3f3ed46fd2a4f620b6d83a0892f3a2
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-22 10:18:21 +0200

    Update gamesshell_deb.sh

--- BASH/gamesshell_deb.sh
+++ BASH/gamesshell_deb.sh
@@ -1,7 +1,7 @@
 #Mise à jour du système 
 sudo apt update && sudo apt full-upgrade -y
 #Installations des dépendances 
-sudo apt install git nala ssh wget curl -y
+sudo apt installgit nala ssh wget curl gettext man-db procps psmisc nano tree ncal x11-apps -y
 #Creation du répertoire de gameshell
 mkdir gameshell
 cd gameshell
