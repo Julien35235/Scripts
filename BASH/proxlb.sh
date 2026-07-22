5dff5faa9fba706a18a5e894323736acaf25d43a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-22 16:52:46 +0200

    Add files via upload

--- BASH/proxlb.sh
+++ BASH/proxlb.sh
@@ -1,3 +1,9 @@
+# Update system
+sudo apt update && sudo apt full-upgrade -y
+
+#Installing the Required Packages
+sudo apt install htop nala btm curl wget -y 
+
 # Add repository signing key
 sudo wget -O /etc/apt/keyrings/proxtools-archive-keyring.gpg \
   https://packages.credativ.com/public/proxtools/archive-keyring.gpg
@@ -8,14 +14,11 @@ https://packages.credativ.com/public/proxtools stable main" \
 | sudo tee /etc/apt/sources.list.d/proxlb.list
 
 # Update & install
-sudo apt-get update
-sudo apt-get -y install proxlb
+sudo apt update
+sudo apt install proxlb -y
 
 # Copy example config
 sudo cp /etc/proxlb/proxlb_example.yaml /etc/proxlb/proxlb.yaml
-
-# Adjust the config to your needs
-sudo vi /etc/proxlb/proxlb.yaml
 
 # Start service
 sudo systemctl start proxlb
