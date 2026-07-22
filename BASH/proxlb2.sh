c278ee363670310efa4fd2493ae3575b2b834e8a
Julien35235 <118825151+Julien35235@users.noreply.github.com>
2026-07-22 17:01:04 +0200

    Add files via upload

---
+++ BASH/proxlb2.sh
@@ -0,0 +1,14 @@
+# Update system
+sudo apt update && sudo apt full-upgrade -y
+
+#Installing the Required Packages
+sudo apt install htop nala btm curl wget -y 
+
+# Retrieving the .deb file for proxlb 
+sudo wget https://github.com/gyptazy/ProxLB/releases/download/v1.1.11/proxlb_1.1.11_all.deb
+
+#Installing ProxLB
+sudo nala instal ./proxlb_1.1.11_all.deb -y
+
+# Start service
+sudo systemctl start proxlb
